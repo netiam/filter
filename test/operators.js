4f371e54d0deb31fc670fa7bdc30cbfd539f808c
Hannes Moser <hannes@impossiblearts.com>
2015-11-02 21:55:30 +0100

    test: add empty query test

--- test/operators.js
+++ test/operators.js
@@ -2,6 +2,11 @@ import filter from '../src/index'
 
 describe('Operators', () => {
 
+  it('should create empty object', () => {
+    const query = ''
+    filter(query).toObject().should.eql({})
+  })
+
   it('should create filter object', () => {
     const query = 'a EQ b AND c EQ d'
     filter(query).toObject().should.deepEqual({
